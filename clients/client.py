bcc0d8a90a84ffc34bbe9aa0e746dbccdfedf616
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-07-16 01:23:24 +0500

    Fixed timing calculation in batch processing

--- clients/client.py
+++ clients/client.py
@@ -15,9 +15,10 @@ class PHClient():
         self.token = loads(result.text)["token"]
     def run_batch_insert(self, batch_size=1000, num_rounds=100):
 
-        s = time()
+        
         # '{"points": [{"test": [{"timestamp": 1752599157688, "value": 5.445}]}]}'
         for j in range(0, num_rounds):
+            s = time()
             batch = {"points": [{self.tag: []}]}
             for i in range(0, batch_size):
                 batch["points"][0][self.tag].append({
@@ -29,8 +30,9 @@ class PHClient():
                     headers={"Accept": "application/json",
                             "Authorization": "Bearer " + self.token})
             #print(result.text)
-        e = time()
-        print("Inserting a batch took " + str(e-s) + " seconds")
+            e = time()
+            print("Inserting " + str(batch_size) + " a batch took " + str(e-s) + " seconds")
+            print(str(batch_size/(e-s)) + " samples per second")
 c = PHClient("http://192.168.1.245:5006", "test")
 c.open()
 c.run_batch_insert(10000, 100)
